389f79ca044c60cb507f72e55394dc9e63a1b9de
JEMOC <ccccjiemo@gmail.com>
2018-08-18 02:04:19 +0800

    reactform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -30,6 +30,7 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     FormsModule,
     AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { user_url } from '../url';
 
 @Component({
@@ -8,6 +9,31 @@ import { user_url } from '../url';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+
+  registForm: FormGroup;
+
+  password: FormControl;
+
+  equalVaildator(group: FormGroup): any {
+    const password = group.get('password') as FormControl;
+    const confirpwd = group.get('confirpwd') as FormControl;
+
+    const isEqule: boolean = (password.value === confirpwd.value);
+    return isEqule ? null : {equal: {info: '密码不一致'}};
+  }
+
+  emailVaildator(control: FormControl): any {
+
+    const emailExp = /^([a-zA-z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
+    const result = emailExp.test(control.value);
+
+    return result ? null : {
+      info: '请输入正确的邮箱'
+    }
+
+  }
+
+
 
   query: any = {
     username: '',
@@ -20,7 +46,7 @@ export class RegisterComponent implements OnInit {
   URL: string = 'http://localhost:81/user/register';
 
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private fb: FormBuilder) {
     const headers = new Headers({
       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
     });
@@ -28,6 +54,18 @@ export class RegisterComponent implements OnInit {
       headers: headers,
       responseType: 'text'
     };
+
+    
+
+    this.registForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(6)]],
+      passwordGroup: this.fb.group({
+        password: ['', Validators.minLength(6)],
+        confirpwd: ['']
+      }, {validator: this.equalVaildator}),
+      email: ['', {validator: this.emailVaildator}]
+    });
+    
    }
 
   ngOnInit() {
